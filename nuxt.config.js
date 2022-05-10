c27f21bd3758c7e49edf3097cf8df8d39aa8da05
Ronin <mdrewj@live.com>
2022-05-10 14:48:38 +0300

    Test2

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  //ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
